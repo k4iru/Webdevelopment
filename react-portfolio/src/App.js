fc093749e537e95a459922cac54473b4875bd3cb
Ky Le <cheung.kyle1@gmail.com>
2018-08-22 15:48:33 -0400

    removed server workers

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 
 import './Assets/css/default.min.css';
-import Header from './components/headerComponent/header';
-import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
 class App extends Component {
   render() {
